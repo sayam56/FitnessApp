7222703331b17a257597cc5b581050476283d3aa
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-01-09 23:56:45 +0600

    fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import './widget_test.dart';
 
-import 'package:FitnessApp/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
